6f66c8c1c3cb6809d572f24283e61b9f69a8cc06
Sergio <pani77sps@gmail.com>
2022-09-22 10:37:31 +0200

    Ejercicio 3
    Instalar 'styled-components'
    Implementar estilos en body (`GlobalStyle`) y textos (`Card`)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-
 //? Importar Component Escena para poder renderizarlo en App()
 import { Escena } from './components/escena/Escena';
+
+//? Importar component Body de Styled.js
+// import { Body } from './components/Styled';
 
 // Importar [array] de textos que será el 'props' a enviar a <Escena />
 import { textos } from './assets/textos';
@@ -10,9 +10,9 @@ import { textos } from './assets/textos';
 function App() {
     return (
         // Usaremos el tag <></> vacío, aunque se podría usar <Fragment></Fragment>
-        <>
+        <div>
             <Escena param={textos} />
-        </>
+        </div>
     );
 }
 
--- src/components/escena/Escena.jsx
+++ src/components/escena/Escena.jsx
@@ -1,3 +1,5 @@
+import { Card } from '../Styled';
+
 // Lo exportamos con nombre explícito (mismo que el archivo), no por default
 // Al contener un único <tag> o elemento, no hace falta envolverlo en un contenedor padre
 
@@ -5,7 +7,11 @@ export const Escena = ({ param }) => {
     // Recorrer array (param) y crear un nuevo <p></p> por cada elemento
     // Poner una 'key' a cada elemento para evitar error en Consola
     const frases = param.map((frase, index) => {
-        return <p key={index}>{frase}</p>;
+        return (
+            <Card>
+                <p key={index}>{frase}</p>
+            </Card>
+        );
     });
 
     // Devolver el conjunto de frases en un contenedor padre vacío, para que no se genere un contenedor (p, div...) que pueda causarnos problemas
