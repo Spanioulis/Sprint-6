33e3bbada0a46a634f3b0181ea981ce960de96d8
Sergio <pani77sps@gmail.com>
2022-09-23 17:28:00 +0200

    Subir archivo modificado StyledComponents (omitido anterior commit)

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -20,19 +20,13 @@ body {
 
 export const Card = styled.p`
     align-items: center;
-    background-color: white;
-    border-radius: 25px;
-    border: 1px black solid;
-    display: flex;
-    font-family: Arial, Helvetica, sans-serif;
-    height: 50px;
-    justify-content: center;
-    margin: 1rem;
-    padding: 0 1rem 0 1rem;
-`;
-export const Card2 = styled.p`
-    align-items: center;
-    background-color: #ecc5fb;
+    background-color: ${({ value }) => {
+        if (value === true) {
+            return '#ecc5fb';
+        } else {
+            return 'aliceblue';
+        }
+    }};
     border-radius: 25px;
     border: 1px black solid;
     display: flex;
@@ -49,21 +43,18 @@ export const BtnContent = styled.div`
 `;
 
 export const Button = styled.button`
-    /* align-items: center; */
     background-color: #d0c9c0;
     border-radius: 5px;
     border: 1px black solid;
-    /* display: flex; */
     font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: large;
     height: 50px;
-    /* justify-content: center; */
     margin: 0.2rem;
     padding: 0 1rem 0 1rem;
     width: 400px;
 
     &:hover {
-        background-color: #7895b2;
+        background-color: #7d6e83;
         color: aliceblue;
     }
 `;
